fd1739752c0329e73b4e8efcfaf3b53da29dd5ff
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-06-13 22:11:32 +0900

    commentout deleted

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,7 +22,6 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
-        # 現在ログイン中のユーザーIDをparamsで取得できるようにしている？⬆️
     
   def set_group
     @group = Group.find(params[:group_id])
