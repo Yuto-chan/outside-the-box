310a5071df36c5ff9158e23d3aee124213fdd9be
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-07-02 22:14:20 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def edit
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    @user = User.find(params[:id]) 
+    # if current_user.update(user_params)
+    #   redirect_to root_path
+    # else
+    #   render :edit
+    # end
   end
 
   def update
