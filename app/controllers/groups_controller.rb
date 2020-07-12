bc5fc01973a31ad991b6aa0d31424a52e99506de
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-07-12 17:12:46 +0900

    unneeded commentout deleted

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,6 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.all
-    # @group1 = Group.first
-    # @group2 = Group.second
-    # @group3 = Group.third
   end
 
 
