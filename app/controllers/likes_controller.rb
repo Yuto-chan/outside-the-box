3a5c258bb53d04d1bb25ddc886d9ee4fae3e9c29
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-08-29 22:48:17 +0900

    unneeded code deleted

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,13 @@ class LikesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    # @like = current_user.likes.create(message_id: params[:message_id])
-    # redirect_back(fallback_location: root_path)
+
     @message = Message.find(params[:message_id])
     @message.iine(current_user)
   end
 
   def destroy
-    # @like = Like.find_by(message_id: params[:message_id], user_id: current_user.id)
-    # @like.destroy
-    # redirect_back(fallback_location: root_path)
+
     @message = Like.find(params[:id]).message
     @message.uniine(current_user)
   end
